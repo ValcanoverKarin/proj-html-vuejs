4da74b17ead6ab87414baa9bedd9c3397b8c8702
karin valcanover <valcanoverkarin@outlook.it>
2021-05-13 00:19:52 +0200

    inizio vue footer

--- src/app.js
+++ src/app.js
@@ -2,6 +2,35 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+
+            crewFirst:
+            "THE DON PEPPE CREW FIRST AND FOREMOST VALUES AN AUTHENTIC, WELL BAKED SLICE OF PIZZA.",
+            
+            addresses: [
+                {
+                    street: "1614 E.Bell Rd #104",
+                    city: "Salerno, AZ 85022",
+                    phone: "(602)867-1010"
+                },
+          
+                {
+                    street: "204 E. Piazzetta Tommaso",
+                    city: "Rome, AZ 85022",
+                    phone: "(358)867-1010"
+                },
+          
+                {
+                    street: "Vale Puglia 54",
+                    city: "Torre Del Greco AZ 85022",
+                    phone: "(359)867-1010"
+                },
+          
+                {
+                    street: "Corso Itali AA",
+                    city: "Naples, AZ 85022",
+                    phone: "(989)867-1010"
+                }
+            ]
         }
     }
 );
